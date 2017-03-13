132aba3f2855a9f886ddf30b1c4668645215af8b
Milo Knowles <mknowles@mit.edu>
2017-03-13 17:26:21 -0400

    Added code to calculate lift coeff

--- uefc.py
+++ uefc.py
@@ -62,6 +62,12 @@ def calculateCoeffDrag(CDA0, S, profDragCoeff, C_L, AR, e):
     t3 = float(C_L**2) / (PI * AR * e)
     return (t1 + t2 + t3)
     
+def calculateCoeffLift(W_fuse, W_wing, W_pay, rho_air, velocity):
+    """
+    Lift = Weight used to derive
+    """
+    C_L = (W_fuse + W_wing + W_pay) / (0.5 * rho_air * velocity**2)
+    return C_L
 
 def calculateWingWeight(tip_chord, root_chord, span, rho, g, tau):
     delta_c = root_chord - tip_chord
@@ -147,6 +153,13 @@ def calculateBankedLoadFactorN(W_fuse, W_wing, W_pay, g, R_turn, S_ref, C_l, rho
     return (first ** (-0.5))
 
 
+def calculateBankedVelocityGivenLoadFactor(G, R, load_factor):
+    """
+    use eqn (20)
+    """
+    v = (G * R * (load_factor**2 - 1)**0.5) ** 0.5
+    return v
+
 
 def calculateRevolutionTime(W_fuse, rho_foam, g, S_ref, AR, CDA0, c_d, T_MAX, \
                             tip_chord, root_chord, span, tau, C_L=0.8, R_turn=12.5, \
@@ -182,7 +195,7 @@ def calculateRevolutionTime(W_fuse, rho_foam, g, S_ref, AR, CDA0, c_d, T_MAX, \
 
 
 def main():
-
+    pass
     # # Note: "make the assumption that c_l = C_L"
     # c_d = calculateProfileDragCoefficient(C_L, c_l_0, c_d_0, c_d_1, c_d_2, c_d_8)
     # print "c_d: ", c_d 
@@ -229,34 +242,133 @@ def main():
     # # print "Rev Time: ", rev_time
 
 
-
-    print "*** OPTIMIZATION ***"
+def calculateMinRevTimeOptimization(AR, S_REF):
+    
+    print "*** OPTIMIZATION *** \n "
 
     print "Optimizing T_rev with AR, S, and lambda_ as free params"
 
-    print "Fixed Parameters: "
-    print "LAMBDA: ", TAPER_RATIO
-
+    print "*** Fixed Parameters: *** "
     # SET S and AR #
-    S_REF = 
-    AR = 
+    # S_REF = 0.228
+    # AR = 10.133
 
+    print "S_REF: %f  AR: %f" % (S_REF, AR)
+
+    print " \n " 
+
+    print "*** Derived Parameters: *** "
     # B and C can be determined by S_REF and AR
     B = (float(S_REF) * AR) ** 0.5
     C = (float(S_REF) / AR) ** 0.5
+    print "B: %f  C: %f" % (B, C)
 
     TIP_CHORD = float(2 * C) / 3
-    ROOT_CHORD = float(4* C) / 3
+    ROOT_CHORD = float(4 * C) / 3
+    print "TIP_CHORD: %f  ROOT_CHORD: %f" % (TIP_CHORD, ROOT_CHORD)
 
     # Now can calculate W_wing
     WING_WEIGHT = calculateWingWeight(TIP_CHORD, ROOT_CHORD, B, RHO_FOAM, G, TAU)
     print "Calculated Wing Weight: ", WING_WEIGHT
 
+    # calculate drag coefficient
+    c_d = calculateProfileDragCoefficient(C_L, c_l_0, c_d_0, c_d_1, c_d_2, c_d_8)
+    print "c_d: ", c_d 
+
+    C_D = calculateCoeffDrag(CDA0, S_REF, c_d, C_L, AR, e)
+    print "C_D: ", C_D
+
+    # Calculate the maximum velocity given T_max
+    V_MAX_THRUST = calculateMaxVelocity(T_MAX, C_D, RHO_AIR)
+    print "V_max due to thrust constraint: ", V_MAX_THRUST
 
 
+    # calculate lift coefficient using maximum velocity constrained by thrust
+    C_L = calculateCoeffLift(WEIGHT_FUSE, WING_WEIGHT, 0, RHO_AIR, V_MAX_THRUST)
 
+    # Using wing weight, can calculate the maxmimum load factor (not constrained by thrust)
+    bending_constrained_N = calculate_Maximum_Load_Factor_Given_DeltaBMax(WEIGHT_FUSE, E_FOAM, TAU, EPSILON, TAPER_RATIO, AR, \
+                                                                          S_REF, 0.1, 0) # Wpay is zero
+
+    print "Bending Constrained Load Factor: ", bending_constrained_N
+
+    V_MAX_BENDING = calculateBankedVelocityGivenLoadFactor(G, 12.5, bending_constrained_N)
+
+    print "Bending Constrained Velocity: ", V_MAX_BENDING
+
+    # COMPARE THE V_MAX due to thrust constrains with the V_MAX due to bending constraints
+    # take the MIN of the 2 #
+    if V_MAX_BENDING < V_MAX_THRUST:
+        print "The flight velocity is bending constrained"
+
+    else:
+        print "The flight velocity is thrust constrained"
+
+    V_MAX = min(V_MAX_BENDING, V_MAX_THRUST)
+
+
+    T_REV = 2 * PI * 12.5 / float(V_MAX)
+    print "T_REV: ", T_REV
+
+    return T_REV
+
+    # max_load_factor_v_constrained = calculateLoadFactorGivenVelocity(V_MAX, 12.5, G)
+    # print "Max Load Factor Given V_max: ", max_load_factor_v_constrained
+
+    # min_rev_time_v_constrained = calculateRevolutionTime(WEIGHT_FUSE, RHO_FOAM, G, S_REF, AR, CDA0, c_d, T_MAX, \
+    #                             TIP_CHORD, ROOT_CHORD, B, TAU, C_L=0.8, R_turn=12.5, N=max_load_factor_v_constrained, W_wing=WING_WEIGHT)
+
+    # print "Min. Rev. Time V-Constrained: ", min_rev_time_v_constrained
+
+
+    # min_rev_time_bending_constrained = calculateRevolutionTime(WEIGHT_FUSE, RHO_FOAM, G, S_REF, AR, CDA0, c_d, T_MAX, \
+    #                                                         TIP_CHORD, ROOT_CHORD, B, TAU, C_L=0.8, R_turn=12.5, \
+    #                                                         N=bending_constrained_N, W_wing=WING_WEIGHT)
+
+    # print "Min. Rev. Time Bending-Constrained: ", min_rev_time_bending_constrained
+
+    # print "\n"
+    # print "*** RESULT ***"
+    # if min_rev_time_v_constrained > min_rev_time_bending_constrained:
+    #     print "The minimum is V-constrained."
+
+    # else:
+    #     print "The minimum is bending constrained."
+
+    # #print "Overall Minimum Rev. Time: %f", max(min_rev_time_v_constrained, min_rev_time_bending_constrained)
+    # return max(min_rev_time_v_constrained, min_rev_time_bending_constrained)
+
+
+def optimizeRevTime():
+
+    aspectRatios = range(1, 50)
+    areas = [0.1 * i for i in range(1,60)]
+
+    optimalRevTime = 1000
+    optimalS_REF = None
+    optimalAR = None
+
+    for aspect_ratio in aspectRatios:
+        for area in areas:
+
+            try:
+                minimumRevTime = calculateMinRevTimeOptimization(aspect_ratio, area)
+            except:
+                print "Problem occurred somewhere due to params..."
+
+            if minimumRevTime < optimalRevTime:
+                optimalRevTime = minimumRevTime
+                optimalS_REF = area
+                optimalAR = aspect_ratio
+
+    print "\n *** FINAL RESULTS ***"
+    print "Optimal Rev. Time: ", optimalRevTime
+    print "Optimal S_ref: ", optimalS_REF
+    print "Optimal AR: ", optimalAR
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+
+    optimizeRevTime()
     
